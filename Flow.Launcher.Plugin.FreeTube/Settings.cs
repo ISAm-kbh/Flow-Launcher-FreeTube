56ca52179f3e1295c841de87531c9b69cc49b46a
ISAm <100785355+ISAm-kbh@users.noreply.github.com>
2025-06-05 14:21:23 -0400

    Remove unused imports

--- Flow.Launcher.Plugin.FreeTube/Settings.cs
+++ Flow.Launcher.Plugin.FreeTube/Settings.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Flow.Launcher.Plugin.FreeTube
+﻿namespace Flow.Launcher.Plugin.FreeTube
 {
     public class Settings
     {
